af06bfdb45a584d61699a0f355de569c44266b0a
Ali-09 <yuzet.ali@gmail.com>
2022-02-14 12:08:04 -0600

    changes script bash

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ git pull
 docker-compose down
 docker stop portafolio_front
 docker rm portafolio_front
-docker rmi portafolio_front
-docker prune
+docker rmi ali-001_front
+docker system prune
 docker-compose up --build -d
