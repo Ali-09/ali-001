7643db1a8eb0c835b2cca1156d75447e2e163435
Ali-09 <yuzet.ali@gmail.com>
2026-07-21 00:23:23 -0600

    style(mobile-spacing): enlarge card paddings, section gaps, title margins, and mobile container scrollability for generous layout freedom

--- components/molecules/TitleGeneral/index.tsx
+++ components/molecules/TitleGeneral/index.tsx
@@ -3,7 +3,7 @@ import { Divider } from 'components'
 
 const TitleGeneral = ({ text } : { text: string }) => {
     return (
-        <div className="w-max items-center">
+        <div className="w-max items-center mb-3 sm:mb-6">
             <h1 className="title">{text}</h1>
             <Divider/>
         </div>
